f1dc35e89367503f7fbf8d4a537e02f003a3592c
abduljelil <abduljaleel.wees@gmail.com>
2022-08-07 13:16:56 +0300

    Lesson5 Part1 Installing tailwind

---
+++ src/views/BlankPage.vue
@@ -0,0 +1,38 @@
+
+
+<template>
+    <header class="grid">
+        <div class="text-4xl">Logo</div>
+        <div>Menu1 Menu2 Menu3</div>
+    </header>
+
+
+    <main>Content</main>
+
+
+    <footer>
+        <div>Logo</div>
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente aperiam illo eaque dolore quae aliquam quas corrupti, laboriosam ducimus sed quo porro et error beatae eum iste deserunt mollitia illum?</div>
+        <div>
+            <h3>Quick Links</h3>
+            <ul>
+                <li>Page1</li>
+                <li>Page2</li>
+                <li>Page3</li>
+                <li>Page4</li>
+            </ul>
+            <h3>Other Links</h3>
+            <ul>
+                <li>Page1</li>
+                <li>Page2</li>
+                <li>Page3</li>
+                <li>Page4</li>
+            </ul>
+        </div>
+    </footer>
+
+
+
+
+
+</template>
